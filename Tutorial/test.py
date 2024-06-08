8311e2184d1a9ff39af39741037aadf944b9ece2
solomonchidera <solochidera10@gmail.com>
2024-06-08 16:02:18 +0100

    a basic tkinter application building using macOS but will work on any OS This is me commiting what am learning

--- Tutorial/test.py
+++ Tutorial/test.py
@@ -5,7 +5,7 @@ from tkinter import ttk # has all the widgets we need
 window = tk.Tk()
 window.title('Demo App')
 window.geometry('800x400') # geometry.(widthxheight)
-label = tk.Label(master = window, text="Hello Moniaar")
+label = tk.Label(master = window, text="Hello Moniaar", font=("Aladin", 16, "bold"))
 label.pack()
 
 # Run not run but run ie start the app
